19e696b328af4086011d7880f6fbd90c649656e8
minhthang165 <minhthangqhqn@gmail.com>
2025-12-07 20:48:33 +0700

    config base + eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
+import './index.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
-import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
 import { Toaster } from 'sonner';
 
-import './index.css';
-import Router from './router';
+import { ClerkProvider } from '@clerk/clerk-react';
+
 import { store } from './redux/store';
+import Router from './router';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
